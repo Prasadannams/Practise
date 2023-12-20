f1b0271572bd2b06414154db0a9769f86389ad52
Prasadannams <prasadannams4@gmail.com>
2023-12-20 16:59:42 +0530

    Practise branches

---
+++ src/FirstCode/Firstclass.java
@@ -0,0 +1,12 @@
+package FirstCode;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello one");
+	}
+
+}
